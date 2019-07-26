2f768a911d43ec2e67f81d1d507f9ea04884e64e
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2019-07-26 10:16:12 -0500

    Added Rocket.Surgery.Nuke
    - Added Azure Pipelines
    - Added GitVersion.yml
    - Added global.json
    - Added NuGet.config
    - Added build scripts
    - Added analyzers

---
+++ .build/Build.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using Rocket.Surgery.Nuke.DotNetCore;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : DotNetCoreBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main() => Execute<Build>(x => x.Default);
+
+    Target Default => _ => _.DependsOn(DotNetCore);
+}
