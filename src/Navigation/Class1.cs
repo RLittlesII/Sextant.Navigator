a468d406c5fa216bd936d394e00ed18a6b9e393f
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-09-18 14:04:16 -0500

    added navigation mediator methods

--- src/Navigation/Class1.cs
+++ src/Navigation/Class1.cs
@@ -1,10 +1,20 @@
-﻿namespace Navigation;
+﻿using System.Reactive;
+
+namespace Navigation;
 
 public interface INavigationMediator
 {
-    IObservable<TResponse> Navigate<TRequest, TResponse>(TRequest request)
-        where TRequest : INavigationRequest
+    IObservable<TResponse> Navigate<TResponse>(Uri uri)
         where TResponse : INavigationResponse;
+
+    IObservable<INavigationResponse> Navigate<TRequest>(TRequest request)
+        where TRequest : INavigationRequest;
+
+    IObservable<INavigationResponse> Navigate<TViewModel>();
+
+    IObservable<INavigationResponse> Navigate<TViewModel>(Uri uri);
+
+    IObservable<INavigationResponse> Navigate<TViewModel>(INavigationRequest<TViewModel> request);
 }
 
 public interface INavigationResponse
@@ -16,6 +26,26 @@ public interface INavigationResponse
     Exception? Exception { get; }
 }
 
-public interface INavigationRequest
+public interface INavigationRequest : IDictionary<string, object>
 {
+    /// <summary>
+    /// Gets the uri that will become the navigation stack.
+    /// </summary>
+    Uri Uri { get; }
+}
+
+public interface INavigationRequest<T> : INavigationRequest
+{
+    T ViewModel { get; }
+}
+
+public interface INavigationContext
+{
+}
+
+public interface INavigationRequestHandler<TRequest, TResponse>
+    where TRequest : INavigationRequest
+    where TResponse : INavigationResponse
+{
+    IObservable<Unit> Initialize();
 }
