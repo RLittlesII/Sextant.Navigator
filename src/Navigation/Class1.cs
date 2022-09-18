f3dc34f18f9fe6a4a6091f42863b625769a22238
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-09-17 22:53:50 -0500

    feature: Mediator Navigation Approach

---
+++ src/Navigation/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace Navigation;
+
+public interface INavigationMediator
+{
+    IObservable<TResponse> Navigate<TRequest, TResponse>(TRequest request)
+        where TRequest : INavigationRequest
+        where TResponse : INavigationResponse;
+}
+
+public interface INavigationResponse
+{
+    string CurrentUrl { get; }
+
+    bool Successful { get; }
+
+    Exception? Exception { get; }
+}
+
+public interface INavigationRequest
+{
+}
